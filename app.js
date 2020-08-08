83c7fd4e0b276ed7ba580ebb877d2475f0ad9593
ilyas chicha <ilyes.chicha@univ-msila.dz>
2020-08-09 00:26:59 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const navToggler = () => {
+  const nav = document.querySelector('.nav-items');
+  const toggler = document.querySelector('#toogler');
+  const togglerImg = document.querySelector('#toogler img');
+  toogler.addEventListener('click', () => {
+    togglerImg.src = (togglerImg.src === image) ? "images/icon-close.svg" : "images/icon-hamburger.svg";
+    nav.classList.toggle('nav-active');
+  });
+}
+navToggler();
+let introImg  = document.querySelector('.intro img');
+let imageSize = () => {
+  introImg.src = (screen.width <= 375) ? "images/bg-intro-mobile.svg" : "images/bg-intro-desktop.svg";
+}
+imageSize();
+window.addEventListener('resize', () => {
+  if (screen.width <= 375) {
+         introImg.src = "images/bg-intro-mobile.svg";
+       } else {
+         introImg.src = "images/bg-intro-desktop.svg";
+       }
+});
